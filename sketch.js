8d7a66345392634b2169a6bd87f1c7afe93e60f5
LeungZa <60365839+LeungZa@users.noreply.github.com>
2020-10-14 18:20:28 -0700

    3.6

--- sketch.js
+++ sketch.js
@@ -1,7 +1,52 @@
+let array = [];
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  background(220);
+
+  strokeWeight(5);
+  noFill();
+
 }
 
 function draw() {
-  background(220);
+
+  if (mouseIsPressed == true) {
+    // stroke(map(mouseX, 0, 600, 0, 255, true));
+    // line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    // line(mouseX, mouseY, pmouseX, pmouseY);
+    // line(mouseX, mouseY, pmouseX, pmouseY);
+    background(0);
+    array.push([mouseX, mouseY]);
+  }
+
+}
+
+function keyTyped() {
+
+  // console.log(`key ${key} is being typed`);
+  // console.log("key " + key + " is being typed");
+  if (key === 's') {
+
+    // console.log(`key s is being typed`)
+    saveCanvas('fileName', 'png');
+  } else if (key === 'd') {
+    // display image
+    // console.log(array);
+    // console.log(array[0]);
+    // console.log(array[0][1]);
+    background(255);
+    beginShape();
+    for (let i = 0; i < array.length - 1; i++) {
+      // console.log(i);
+      // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+      curveVertex(array[i][0], array[i][1])
+    }
+    endShape();
+    // line(array[0][0], array[1][1], array[2][0], array[2][1]);
+
+  }
+
+  return false;
 }
